53ae7373d3eb5c47829977760cbffa81a84ba87b
Gyeong-Min Lee <tyler7688@gmail.com>
2025-04-07 11:18:50 -0700

    Unused imports

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,20 +1,9 @@
 "use client";
 
-import {
-	BookOpen,
-	Bot,
-	Frame,
-	GalleryVerticalEnd,
-	Map,
-	PieChart,
-	Settings2,
-	SquareTerminal,
-} from "lucide-react";
 import * as React from "react";
 
 import SidebarBodyContents from "@/app/(components)/sidebar/sidebar-body-contents";
 import SidebarHeaderContents from "@/app/(components)/sidebar/sidebar-header-contents";
-import { NavUser } from "@/components/nav-user";
 import {
 	Sidebar,
 	SidebarContent,
@@ -25,128 +14,7 @@ import {
 import { UserButton } from "@clerk/nextjs";
 import { ThemeToggle } from "./ui/theme/theme-toggle";
 
-// This is sample data.
-const data = {
-	user: {
-		name: "shadcn",
-		email: "m@example.com",
-		avatar: "/avatars/shadcn.jpg",
-	},
-	teams: [
-		{
-			name: "Michat",
-			logo: GalleryVerticalEnd,
-			plan: "Friends",
-		},
-	],
-	navMain: [
-		{
-			title: "Playground",
-			url: "#",
-			icon: SquareTerminal,
-			isActive: true,
-			items: [
-				{
-					title: "History",
-					url: "#",
-				},
-				{
-					title: "Starred",
-					url: "#",
-				},
-				{
-					title: "Settings",
-					url: "#",
-				},
-			],
-		},
-		{
-			title: "Models",
-			url: "#",
-			icon: Bot,
-			items: [
-				{
-					title: "Genesis",
-					url: "#",
-				},
-				{
-					title: "Explorer",
-					url: "#",
-				},
-				{
-					title: "Quantum",
-					url: "#",
-				},
-			],
-		},
-		{
-			title: "Documentation",
-			url: "#",
-			icon: BookOpen,
-			items: [
-				{
-					title: "Introduction",
-					url: "#",
-				},
-				{
-					title: "Get Started",
-					url: "#",
-				},
-				{
-					title: "Tutorials",
-					url: "#",
-				},
-				{
-					title: "Changelog",
-					url: "#",
-				},
-			],
-		},
-		{
-			title: "Settings",
-			url: "#",
-			icon: Settings2,
-			items: [
-				{
-					title: "General",
-					url: "#",
-				},
-				{
-					title: "Team",
-					url: "#",
-				},
-				{
-					title: "Billing",
-					url: "#",
-				},
-				{
-					title: "Limits",
-					url: "#",
-				},
-			],
-		},
-	],
-	projects: [
-		{
-			name: "Design Engineering",
-			url: "#",
-			icon: Frame,
-		},
-		{
-			name: "Sales & Marketing",
-			url: "#",
-			icon: PieChart,
-		},
-		{
-			name: "Travel",
-			url: "#",
-			icon: Map,
-		},
-	],
-};
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-	// const paths = useNavigation();
-	// const { isMobile } = useSidebar();
 	return (
 		<Sidebar collapsible="icon" {...props}>
 			<SidebarHeader>
@@ -156,7 +24,6 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 				<SidebarBodyContents />
 			</SidebarContent>
 			<SidebarFooter>
-				{/* <NavUser user={data.user} /> */}
 				<ThemeToggle />
 				<UserButton />
 			</SidebarFooter>
