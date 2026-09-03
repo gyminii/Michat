eb381c5ab9e77d4e6a0cd92db04077a69b166ee1
Tyler Lee <tyler@builtspace.com>
2026-09-03 19:23:44 +0900

    Protect /chats routes in proxy matcher

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 // const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
-const isProtectedRoute = createRouteMatcher(["/chat(.*)", "/friends(.*)"]);
+const isProtectedRoute = createRouteMatcher(["/chats(.*)", "/friends(.*)"]);
 // const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
 export default clerkMiddleware(async (auth, req) => {
 	if (isProtectedRoute(req)) await auth.protect();
